acfa43ebc790b18a440de0631c900c4c5bfbb9bb
miya0001 <miya@wpist.me>
2015-11-04 05:13:34 +0900

    add travis support

--- bin/convert.rb
+++ bin/convert.rb
@@ -1,3 +1,3 @@
-Dir.glob('../shape/*.shp') {|path|
+Dir.glob('./data/shape/*.shp') {|path|
   print "ogr2ogr -f 'GeoJSON' -lco 'ENCODING=LDID/19'  #{File.basename(path).sub('shp', 'geojson')} #{path}\n"
 }
