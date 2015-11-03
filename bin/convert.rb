3216a5a0726e58ec68d556f9c9e6c36b76ed064b
miya0001 <miya@wpist.me>
2015-11-04 05:22:08 +0900

    some fix

--- bin/convert.rb
+++ bin/convert.rb
@@ -1,3 +1,3 @@
 Dir.glob('./data/shape/*.shp') {|path|
-  print "ogr2ogr -f 'GeoJSON' -lco 'ENCODING=LDID/19'  #{File.basename(path).sub('shp', 'geojson')} #{path}\n"
+  print "ogr2ogr -f 'GeoJSON' -lco 'ENCODING=LDID/19'  #{File.join( "data/geojson", File.basename( path ).sub( 'shp', 'geojson' ) )} #{path}\n"
 }
